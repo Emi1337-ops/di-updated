8ef5f530c5ce6e2fca01a9c198c23ba17bb70066
Emi1337-ops <zaicev2400@mail.ru>
2024-12-18 19:12:13 +0500

    Create Project and Add Code from Past Task

---
+++ TagsCloudContainer/Visualizers/ImageCreater.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TagsCloudContainer.Visualizers
+{
+    public class ImageCreater
+    {
+        private readonly int Width;
+        private readonly int Height;
+
+        public ImageCreater(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public void Generate(IEnumerable<Rectangle> items, string directory)
+        {
+            var image = new Bitmap(Width, Height);
+            var g = Graphics.FromImage(image);
+            var pen = new Pen(Brushes.AliceBlue, 2);
+            var count = 0;
+            foreach (var item in items)
+            {
+                count++;
+                g.DrawRectangle(pen, item);
+            }
+
+            image.Save(directory, ImageFormat.Jpeg);
+
+        }
+    }
+}
