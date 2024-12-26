d658b56baed64c2d7a13a2def4884cd3b02c6e27
Emi1337-ops <zaicev2400@mail.ru>
2024-12-26 21:19:29 +0500

    Remove comments

--- ConsoleClient/ConsoleClient.cs
+++ ConsoleClient/ConsoleClient.cs
@@ -3,65 +3,6 @@
 //using TagsCloudContainer;
 //using TagsCloudContainer.FileReaders;
 //using TagsCloudContainer.Filters;
-//using TagsCloudContainer.Layouters;
-//using TagsCloudContainer.Parsers;
-//using TagsCloudContainer.Visualizers;
-//using TagsCloudContainer.WordSizer;
-
-//public class ConsoleClient
-//{
-//    [Option("--input", "Path to the input file.", CommandOptionType.SingleValue)]
-//    public string InputDirectory { get; set; }
-
-//    [Option("--output", "Path to the output file.", CommandOptionType.SingleValue)]
-//    public string OutputDirectory { get; set; }
-
-//    [Option("--width", "Width of the picture.", CommandOptionType.SingleValue)]
-//    public int PictureWidth { get; set; }
-
-//    [Option("--height", "Height of the picture.", CommandOptionType.SingleValue)]
-//    public int PictureHeight { get; set; }
-
-//    [Option("--font", "Font for the picture text.", CommandOptionType.SingleValue)]
-//    public string Font { get; set; }
-
-//    [Option("--stopwords", "Comma-separated list of stop words.", CommandOptionType.SingleValue)]
-//    public string StopWordsInput { get; set; }
-
-//    [Option("--rightwords", "Comma-separated list of right words.", CommandOptionType.SingleValue)]
-//    public string RightWordsInput { get; set; }
-
-//    [Option("--colors", "Comma-separated list of colors in ARGB format (e.g., 255,0,0,255).", CommandOptionType.SingleValue)]
-//    public string ColorsInput { get; set; }
-
-//    public string[] StopWords => StopWordsInput?.Split(',').Select(x => x.ToLower()).ToArray() ?? Array.Empty<string>();
-
-//    public string[] RightWords => RightWordsInput?.Split(',').Select(x => x.ToLower()).ToArray() ?? Array.Empty<string>();
-
-//    public string[] PictureColors => ColorsInput?.Split(',') ?? Array.Empty<string>();
-
-//    public static int Main(string[] args)
-//    {
-//        return CommandLineApplication.Execute<ConsoleClient>(args);
-//    }
-
-//    private void OnExecute()
-//    {
-//        var config = new Config(
-//            InputDirectory is null ? Constants.InputDirectory : InputDirectory,
-//            OutputDirectory is null ? Constants.OutputDirectory : OutputDirectory,
-//            PictureWidth == default ? Constants.PictureWidth : PictureWidth,
-//            PictureHeight == default ? Constants.PictureHeight : PictureHeight,
-//            Font is null ? Constants.Font : Font,
-//            StopWords is null ? Constants.StopWords : StopWords,
-//            RightWords is null ? Constants.RightWords : RightWords,
-//            PictureColors is null ? Constants.PictureColors : PictureColors
-//           );
-
-//        TagsCloudContainer.Program.Main(config);
-//    }
-//}
-
 using McMaster.Extensions.CommandLineUtils;
 using Autofac;
 using TagsCloudContainer;
--- TagsCloudContainer/ApplicationRunner.cs
+++ TagsCloudContainer/ApplicationRunner.cs
@@ -6,52 +6,6 @@ using TagsCloudContainer.Layouters;
 using TagsCloudContainer.Parsers;
 using TagsCloudContainer.Visualizers;
 using TagsCloudContainer.WordSizer;
-//using TagsCloudContainer.FileReaders;
-//using TagsCloudContainer.Filters;
-//using TagsCloudContainer.Layouters;
-//using TagsCloudContainer.Parsers;
-//using TagsCloudContainer.Visualizers;
-//using TagsCloudContainer.WordSizer;
-
-//namespace TagsCloudContainer;
-//public static class Program
-//{
-//    public static void Main()
-//    {
-//    }
-//    public static void Main(Config config)
-//    {
-//        var builder = new ContainerBuilder();
-//        builder.RegisterInstance(config).As<Config>();
-//        builder.RegisterType<TxtFileReader>().As<IReader>();
-//        builder.RegisterType<WordsFilter>().As<IFilter>();
-//        builder.RegisterType<SimpleParser>().As<IParser>();
-//        builder.RegisterType<SimpleSizer>().As<ISizer>();
-//        builder.RegisterType<CircularCloudLayouter>().As<ILayouter>();
-//        builder.RegisterType<ImageVisualizer>().As<IVisualizer>();
-//        var container = builder.Build();
-
-//        var reader = container.Resolve<IReader>();
-//        var parser = container.Resolve<IParser>();
-//        var sizer = container.Resolve<ISizer>();
-//        var layouter = container.Resolve<ILayouter>();
-//        var visualizer = container.Resolve<IVisualizer>();
-
-//        visualizer.GenerateImage(
-//            layouter.GetLayout(
-//                sizer.GetSizes(
-//                    parser.Parse(
-//                        reader.Read(
-//                            config.InputDirectory
-//                        )
-//                    )
-//                )
-//            )
-//        );
-//    }
-//}
-
-
 
 public class ApplicationRunner : IApplicationRunner
 {
