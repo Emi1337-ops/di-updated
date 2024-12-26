03f49ddbe04f3124beb6eb721d69af719c5c8515
Emi1337-ops <zaicev2400@mail.ru>
2024-12-26 20:20:19 +0500

    Add Parser Tests

--- TagCloudContainerTests/ParserTests.cs
+++ TagCloudContainerTests/ParserTests.cs
@@ -1,14 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentAssertions;
+using TagsCloudContainer;
 using TagsCloudContainer.FileReaders;
+using TagsCloudContainer.Filters;
+using TagsCloudContainer.Parsers;
 
 namespace TagCloudContainerTests;
 
 [TestFixture]
 public class ParserTests
 {
+    private Config config;
+    private WordsFilter defaultFilter;
 
+    [SetUp]
+    public void Setup()
+    {
+        config = new Config();
+        defaultFilter = new WordsFilter(config);
+        var folderPath = Path.Combine(Constants.ProjectDirectory, "TagCloudConrainerTests\\Files");
+    }
+
+    [TestCase("Шла Саша шоссе")]
+    [TestCase("Бублик Печенька")]
+    [TestCase("Автобот качели")]
+    [TestCase("мимино квадрат малевич")]
+    public void Parser_CorrectWithSimpleWords(string text)
+    {
+        var parser = new SimpleParser(defaultFilter);
+
+        var parsed = parser.Parse(text);
+
+        foreach (var word in text.Split(' '))
+        {
+            parsed.ContainsKey(word.ToLower()).Should().BeTrue();
+        }
+    }
+
+    [TestCase("Шла %1--Саша @#^%шоссе", "шла саша шоссе")]
+    [TestCase("Бублик@ -Печенька", "бублик печенька")]
+    [TestCase("()()()Автобот _+качели", "автобот качели")]
+    [TestCase("мимино% квадрат$ #малевич++", "мимино квадрат малевич")]
+    [TestCase("%:(*?кролик:%**)", "кролик")]
+    public void Parser_CorrectWithNonLettericSymbols(string text, string correct)
+    {
+        var parser = new SimpleParser(defaultFilter);
+
+        var parsed = parser.Parse(text);
+
+        foreach (var word in correct.Split(' '))
+        {
+            parsed.ContainsKey(word).Should().BeTrue();
+        }
+    }
+
+
+    [TestCase("шла я и саша по шоссе", "шла саша шоссе")]
+    [TestCase("бублик под печенькой", "бублик печенькой")]
+    [TestCase("автобот от качелей", "автобот качелей")]
+    [TestCase("мимино оно как квадрат малевича", "мимино квадрат малевича")]
+    public void Parser_FilterStopWords(string text, string correct)
+    {
+        var parser = new SimpleParser(defaultFilter);
+
+        var parsed = parser.Parse(text);
+
+        foreach (var word in correct.Split(' '))
+        {
+            parsed.ContainsKey(word).Should().BeTrue();
+        }
+    }
+
+    [Test]
+    public void Parser_GiveCorrectWeightToWords()
+    {
+        var parser = new SimpleParser(defaultFilter);
+        var text = "один два два три три три четыре четыре четыре четыре";
+        var correct = new Dictionary<string, int>
+        {
+            { "один", 1},
+            { "два", 2},
+            { "три", 3},
+            { "четыре", 4},
+        };
+
+        var parsed = parser.Parse(text);
+
+        parsed.Should().BeEquivalentTo(correct);
+    }
 }
--- TagCloudContainerTests/SizerTests.cs
+++ TagCloudContainerTests/SizerTests.cs
@@ -5,49 +5,4 @@ namespace TagCloudContainerTests;
 [TestFixture]
 public class SizerTests
 {
-    private static readonly VerifySettings Settings = new();
-    private IReader Reader;
-    private string FolderPath;
-
-    [SetUp]
-    public void Setup()
-    {
-        Settings.UseDirectory("Snapshots");
-        var projectDirectory =
-            Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\"));
-        FolderPath = Path.Combine(projectDirectory, "Files");
-    }
-
-    [Test]
-    public Task Reader_TxtReading()
-    {
-        Reader = new TxtFileReader();
-
-        var filePath = Path.Combine(FolderPath, "wordsTXT.txt");
-        var actual = Reader.Read(filePath);
-
-        return Verify(actual, Settings);
-    }
-
-    [Test]
-    public Task Reader_DocxReading()
-    {
-        Reader = new DocxFileReader();
-
-        var filePath = Path.Combine(FolderPath, "wordsDOCX.docx");
-        var actual = Reader.Read(filePath);
-
-        return Verify(actual, Settings);
-    }
-
-    [Test]
-    public Task Reader_DocReading()
-    {
-        Reader = new DocFileReader();
-
-        var filePath = Path.Combine(FolderPath, "wordsDOC.doc");
-        var actual = Reader.Read(filePath);
-
-        return Verify(actual, Settings);
-    }
 }
